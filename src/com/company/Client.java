78d558ec3f6677a085f19eb88f9ec3710e62248d
Pongtawan Kaenprom <pongtawan2541@gmail.com>
2021-02-20 01:15:47 +0700

    update

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,21 +1,30 @@
 package com.company;
 
-import java.io.IOException;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Client {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
         Socket clientSocket = new Socket();
         System.out.println("Connecting");
         clientSocket.connect(new InetSocketAddress("127.0.0.1", 8080));
         System.out.printf("Connected from port %d\n", clientSocket.getLocalPort());
 
-        clientSocket.getOutputStream().write("Hello".getBytes());
-        clientSocket.getOutputStream().flush();
-        clientSocket.close();
+        Scanner scanner = new Scanner(clientSocket.getInputStream());
+        while (true) {
+            System.out.print("Enter message: ");
+            String message = sc.nextLine();
+            clientSocket.getOutputStream().write((message + "\n").getBytes());
+            clientSocket.getOutputStream().flush();
+            if (scanner.hasNextLine()) {
+                String msg = scanner.nextLine();
+                System.out.println(msg);
+            }
+        }
     }
 }
--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -1,23 +1,56 @@
 package com.company;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Server {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         ServerSocket serverSocket = new ServerSocket();
         serverSocket.bind(new InetSocketAddress(8080));
         System.out.println("Listening to 8080");
-        Socket clientSocket = serverSocket.accept();
-        System.out.printf("Client connected %s:%d\n", clientSocket.getInetAddress().getHostAddress(), clientSocket.getPort());
 
-        Scanner scanner = new Scanner(clientSocket.getInputStream());
-        System.out.println(scanner.nextLine());
-        clientSocket.close();
-
+        ArrayList<Socket> clientSocketList = new ArrayList<>();
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.printf("Client connected %s:%d in %d\n", clientSocket.getInetAddress().getHostAddress(),
+                    clientSocket.getPort(), clientSocket.getLocalPort());
+            Thread thread = new Thread(new ClientHandler(clientSocket, clientSocketList));
+            clientSocketList.add(clientSocket);
+            thread.start();
+        }
     }
 }
+
+class ClientHandler implements Runnable {
+    private Socket clientSocket;
+    private ArrayList<Socket> clientSocketList;
+
+    public ClientHandler(Socket clientSocket, ArrayList<Socket> clientSocketList) {
+        this.clientSocket = clientSocket;
+        this.clientSocketList = clientSocketList;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Scanner scanner = new Scanner(clientSocket.getInputStream());
+            while (true) {
+                if (scanner.hasNextLine()) {
+                    String message = scanner.nextLine();
+                    for (Socket client : clientSocketList) {
+                        client.getOutputStream().write((message + "\n").getBytes());
+                        client.getOutputStream().flush();
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+
+        }
+    }
+}
+
